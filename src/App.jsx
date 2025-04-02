aff50f680f102ebd0b099b3a1bbd35be15366dbe
CedricA <cedric.j.aoun@gmail.com>
2025-04-02 12:53:47 +0200

    make initial movie API call

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { Container } from 'react-bootstrap'
 import './App.css'
-import MovieList from './components/MovieList/MovieList'
 import Movie from './components/SingleMovie/Movie'
 
 function App() {
   return (
     <Container>
-      <MovieList></MovieList>
       <Movie></Movie>
     </Container>
   )
--- src/components/SingleMovie/Movie.jsx
+++ src/components/SingleMovie/Movie.jsx
@@ -1,15 +1,59 @@
-import { Card } from "react-bootstrap"
+import { useEffect, useState } from "react";
+import { Card } from "react-bootstrap";
+import './movie.css'
 
-function Movie({ title, releaseDate, imgURL, desc, averageRating, totalVotes }) {
+function Movie() {
+  const [errorMsg, setErrorMsg] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [randomMovie, setRandomMovie] = useState({});
+  const {
+    movie_id = '',
+    original_title = '',
+    overview = '',
+    vote_average = 0,
+    vote_count = 0,
+    poster_path = '',
+    release_date = '',
+  } = randomMovie?.[0] || {};
+
+  const fetchMovie = async () => {
+    try {
+      const response = await fetch("https://jsonfakery.com/movies/random/1");
+
+      if(!response.ok) {
+        throw new Error(`HTTP Error: ${response.status} with message: ${response.statusText}`);
+      }
+      const data = await response.json();
+      console.log(data);
+      setRandomMovie(data);
+      setIsLoading(false);
+    } catch (err) {
+      setErrorMsg('An error occured while choosing a random movie');
+      console.error(err);
+    } 
+  }
+
+  useEffect(() => {
+    fetchMovie();
+  }, []);
+
+  console.log(movie_id);
+
+  if(errorMsg) return <p className="text-danger text-center">{errorMsg}</p>
+
+  if(isLoading) return <p className="text-primary text-center">Loading...</p>
+
   return (
-    <Card>
-      <Card.Img variant="top" src={imgURL} />
+    <Card id={movie_id}>
+      <Card.Img variant="top" src={poster_path}/>
       <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Subtitle>Date de sortie: {releaseDate}</Card.Subtitle>
+        <Card.Title>{original_title}</Card.Title>
+        <Card.Subtitle>Date de sortie: {release_date}</Card.Subtitle>
         <Card.Text>
-          {desc}
-          <p>Note moyenne: {averageRating} ({totalVotes})</p>
+          {overview}
+        </Card.Text>
+        <Card.Text>
+          Note moyenne: {vote_average} ({vote_count} votes)
         </Card.Text>
       </Card.Body>
     </Card>
